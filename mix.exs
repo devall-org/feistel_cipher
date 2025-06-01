c074b3e0cffcd375a49aa687209a45e0d1b45772
Jechol Lee <jechol@devall.org>
2025-06-02 07:32:08 +0900

    Integrate xor and mul parameters into seed.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FeistelCipher.MixProject do
   def project do
     [
       app: :feistel_cipher,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.17",
       consolidate_protocols: Mix.env() not in [:dev, :test],
       start_permanent: Mix.env() == :prod,
