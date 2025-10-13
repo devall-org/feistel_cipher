11bf53bf3a7744b67b45e33ac218d64ac54b53f8
Jechol Lee <jechol@devall.org>
2025-10-13 09:16:53 +0900

    test: add Ecto Schema integration tests
    - Add Post schema example in test/support
    - Add comprehensive integration tests verifying README examples
    - Tests cover: basic insert, multiple inserts, determinism, query by encrypted id
    - All 32 tests passing (28 existing + 4 new)

---
+++ test/support/post.ex
@@ -0,0 +1,16 @@
+defmodule FeistelCipher.Post do
+  @moduledoc """
+  Test schema for FeistelCipher integration testing
+  """
+  use Ecto.Schema
+
+  # Hide seq in API responses
+  @derive {Jason.Encoder, except: [:seq]}
+
+  @primary_key {:id, :id, autogenerate: false, read_after_writes: true}
+  schema "posts" do
+    field :seq, :id, read_after_writes: true
+    field :title, :string
+  end
+end
+
