6ad7c7c08be5ffd795d0fc9c84f6a61cbc80e0c7
Jechol Lee <jechol@devall.org>
2025-12-11 13:01:20 +0900

    Harden Feistel cipher with MD5-based round function
    BREAKING CHANGE: This version is incompatible with data encrypted by previous versions.
    
    - Replace simple XOR/multiplication round function with MD5-based hash function
    - This prevents algebraic attacks where consecutive plaintext-ciphertext pairs could be used to recover salt/key
    - Similar approach to Rust's feistel-permutation-rs library which uses SipHash
    - Update golden test values for the new algorithm
    - Bump version to 0.14.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FeistelCipher.MixProject do
   def project do
     [
       app: :feistel_cipher,
-      version: "0.13.4",
+      version: "0.14.0",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       consolidate_protocols: Mix.env() not in [:dev, :test],
